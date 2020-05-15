54dbc61a4052007219aca576b3dde437a4d02ef0
Hyungkyu Ham <hhk971@postech.ac.kr>
2020-05-15 14:04:56 +0900

    control logic

--- lab5/control.v
+++ lab5/control.v
@@ -1,46 +1,34 @@
-module control(instr, jal, branch, mem_read, mem_write, alu_src, reg_write, mem_to_reg);
-    input [3:0] instr;
-    output reg jal;
-    output reg branch;
-    output reg mem_read;
-    output reg mem_write;
+`include "opcodes.v"
+`define WORD_SIZE 16    // data and address word size
+
+module control(instruction, alu_src, alu_op, reg_dest, mem_write, mem_read, reg_write, mem_to_reg);
+    input [`WORD_SIZE-1:0] instruction;
     output reg alu_src;
+    output reg alu_op;
+    output reg reg_dest;
+    output reg mem_write;
+    output reg mem_read;
     output reg reg_write;
     output reg mem_to_reg;
  
-    wire rtype;
-    wire itype;
-    wire lw;
-    wire sw;
-    wire br;
-    wire jp;
-
-    assign rtype = instr[0] & instr[1] & instr[2] & instr[3]; // 15
-    assign itype = ~instr[3] | (~instr[0] & ~instr[1] & ~instr[2] & instr[3]); // 0~8
-    assign lw = ~instr[3] &  instr[2] &  instr[1] &  instr[0]; // 7
-    assign sw =  instr[3] & ~instr[0] & ~instr[1] & ~instr[2]; // 8
-    assign br = ~instr[3] & ~instr[2]; //0 ~3
-    assign jp =  instr[3] & ~instr[2] & (instr[1] ^ instr[0]); // 9, 10
-
-    initial begin
-        jal <= 0;
-        branch <= 0;
-        mem_read <= 0;
-        mem_write <= 0;
-        alu_src <= 0;
-        reg_write <= 0;
-        mem_to_reg <= 0;
-    end
+    wire ; [3:0] opcode;
+    wire [5:0] alu_instruction;
+    wire rtype, itype, jtype, load, store, branch;
+    assign opcode = instruction[15:12];
+    assign alu_instruction = instruction[5:0];
+    assign rtype = opcode[0] & opcode[1] & opcode[2] & opcode[3]; // 15
+    assign itype = ~opcode[3] | (~opcode[0] & ~opcode[1] & ~opcode[2] & opcode[3]); // 0~8
+    assign load = ~opcode[3] &  opcode[2] &  opcode[1] &  opcode[0]; // 7
+    assign store =  opcode[3] & ~opcode[0] & ~opcode[1] & ~opcode[2]; // 8
+    assign branch = ~opcode[3] & ~opcode[2]; //0 ~3
+    assign jtype =  opcode[3] & ~opcode[2] & (opcode[1] ^ opcode[0]); // 9, 10
 
     //Combinational logic for output
     always @(*) begin
-        jal = jp;
-        branch = br;
-        mem_write = sw;
-        alu_src = itype;
-        reg_write = (jp &(instr[3] & ~instr[2] &instr[1] & ~instr[0] )) | (itype & ~sw) | rtype;
-        mem_to_reg = lw;
-        mem_read = lw;
+        mem_read = load;
+        mem_to_reg = load;
+        mem_write = store;
+        
     end
 
 endmodule
