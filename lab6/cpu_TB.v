ba49f0de21972bdb3025be5e471117859d7f5f95
cskhy16 <cskhy16@postech.ac.kr>
2020-05-31 17:59:06 +0900

    two delay port

--- lab6/cpu_TB.v
+++ lab6/cpu_TB.v
@@ -21,10 +21,12 @@ module cpu_TB();
 	wire [`WORD_SIZE-1:0] num_inst;		// number of instruction during execution
 	wire [`WORD_SIZE-1:0] output_port;	// this will be used for a "WWD" instruction
 	wire is_halted;				// set if the cpu is halted
+	wire read_ack;
+	wire write_ack;
 
 	// instantiate the unit under test
-	cpu UUT (clk, reset_n, readM1, address1, data1, readM2, writeM2, address2, data2, num_inst, output_port, is_halted);
-	Memory NUUT(!clk, reset_n, readM1, address1, data1, readM2, writeM2, address2, data2);
+	cpu UUT (clk, reset_n, readM1, address1, data1, readM2, writeM2, address2, data2, num_inst, output_port, is_halted, read_ack, write_ack);
+	Memory NUUT(!clk, reset_n, readM1, address1, data1, readM2, writeM2, address2, data2, req_mem_read, req_mem_write, read_ack, write_ack);
 
 	// initialize inputs
 	initial begin
